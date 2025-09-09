dc6652dba5796833ad736c6f1c3fdbde7dc95783
J salah <jaboursalahddine@gmail.com>
2025-09-09 04:55:59 +0200

    Buil API Gatewayserver using Spring Cloud Gateway

---
+++ gatewayserver/src/test/java/com/mybank/gatewayserver/GetewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybank.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
