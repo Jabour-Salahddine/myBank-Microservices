fcad57e10a3b9e788aea5b26e85ad171b9dd4d74
J salah <jaboursalahddine@gmail.com>
2025-09-09 22:09:55 +0200

    build custom routes for the gateway server. we used predifined predicates and filters

--- gatewayserver/src/main/java/com/mybank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/mybank/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.mybank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -9,5 +14,34 @@ public class GatewayserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
+
+
+    // this methode is used to configure our chosen routes for the gateway server. we used predifined predicates and filters
+	@Bean
+	public RouteLocator myBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/mybank/accounts/**")
+						.filters( f -> f.rewritePath("/mybank/accounts/(?<segment>.*)","/${segment}")
+						.addResponseHeader("X-ResponseTime", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS"))
+
+				.route(p -> p
+						.path("/mybank/loans/**")
+						.filters( f -> f.rewritePath("/mybank/loans/(?<segment>.*)","/${segment}")
+						.addResponseHeader("X-ResponseTime", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+
+				.route(p -> p
+						.path("/mybank/cards/**")
+						.filters( f -> f.rewritePath("/mybank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-ResponseTime", LocalDateTime.now().toString()))
+						.uri("lb://CARDS")).build();
+
+
+	}
+
+
+
 
 }
